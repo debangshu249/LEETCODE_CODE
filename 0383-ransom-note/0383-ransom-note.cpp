79103f77bd678c72f8aede2a6c044893e90fb319
debangshu249 <161063931+debangshu249@users.noreply.github.com>
2026-07-02 17:42:12 +0530

    Time: 2 ms (59.3%), Space: 12.1 MB (11.11%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+
+    bool fun(unordered_map<char,int> &need,unordered_map<char,int> &have){
+        for(auto i : need){
+            char c = i.first;
+            int fneed = i.second;
+            int fhave = have[c];
+            if(fhave < fneed)
+            return false;
+        }
+            return true;
+    }
+    bool canConstruct(string ransomNote, string magazine) {
+    unordered_map<char,int> need;
+    unordered_map<char,int> have;
+    for(int i = 0;i < ransomNote.size();i++)
+    need[ransomNote[i]]++;
+    for(int i = 0;i < magazine.size();i++)
+    have[magazine[i]]++;
+    return fun(need,have);
+    }
+};
